3d28aae72eabd446f7077dab053273f27c06e10f
eliily00 <wjddms0700@gmail.com>
2022-03-28 18:39:36 +0900

    22.03.28 : [10718 - We love kriii]

---
+++ Algorithm/Baekjoon/weeks1/baekjoon_10718.java
@@ -0,0 +1,13 @@
+package baekjoon.weeks1;
+
+/* 
+[백준 알고리즘 - 입출력과 사칙연산 단계 2]
+시간 : 120ms
+코드 길이 145B
+*/
+
+public class baekjoon_10718 {
+    public static void main(String[] args) {
+        System.out.printf("강한친구 대한육군/n강한친구 대한육군");
+    }
+}
