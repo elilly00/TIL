ea5e83816f0148feb4dc015725c8582b65a3b562
eliily00 <wjddms0700@gmail.com>
2022-03-29 19:31:24 +0900

    22.03.29 : [10172 - 개]

---
+++ Algorithm/Baekjoon/weeks1/Baekjoon_10172.java
@@ -0,0 +1,20 @@
+package baekjoon.weeks1;
+
+/*
+[백준 알고리즘 - 입출력과 사칙연산 단계 4]
+메모리 : 13920KB
+시간 : 120ms
+코드 길이 : 283B
+풀이 방법 : 큰 따옴표나 백슬래시는 단독으로 출력할 수 없다. 
+            그렇기 때문에 Escape Sequence를 이용해 출력한다.
+*/
+
+public class Baekjoon_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
