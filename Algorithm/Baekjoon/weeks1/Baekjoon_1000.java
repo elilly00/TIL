8bd9313a0d91ae4b5dc3c8eefcd82d797be45440
eliily00 <wjddms0700@gmail.com>
2022-03-29 19:31:46 +0900

    22.03.29 : [1000 - A+B]

---
+++ Algorithm/Baekjoon/weeks1/Baekjoon_1000.java
@@ -0,0 +1,23 @@
+package baekjoon.weeks1;
+
+import java.util.Scanner;
+/*
+[백준 알고리즘 - 입출력과 사칙연산 단계 5]
+메모리 : 17720KB
+시간 : 204ms
+코드 길이 : 236B
+ */
+
+public class Baekjoon_1000 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+
+        sc.close();
+    }
+}
