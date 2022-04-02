d248eec7e22fc2c3697e21f138842a6db9b682ad
eliily00 <wjddms0700@gmail.com>
2022-04-02 23:31:11 +0900

    22.04.02 : [10430 - 나머지]

---
+++ Algorithm/Baekjoon/weeks1/Baekjoon_10430.java
@@ -0,0 +1,27 @@
+package baekjoon.week1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+/*
+[백준 알고리즘 - 입출력과 사칙연산 단계 12 - 나머지]
+메모리 : 14216KB
+시간 : 120ms
+코드 길이 : 690B
+ */
+public class Baekjoon_10430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        System.out.println((A+B)%C);
+        System.out.println((A%C + B%C)%C);
+        System.out.println((A*B)%C);
+        System.out.println((A%C * B%C)%C);
+    }
+}
