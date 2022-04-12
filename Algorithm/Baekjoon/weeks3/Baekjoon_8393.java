9fc113d174c73af59e904492306817506917c061
eliily00 <wjddms0700@gmail.com>
2022-04-13 00:00:08 +0900

    22.04.12 : [8393 - 합] Modified File

--- Algorithm/Baekjoon/weeks3/Baekjoon_8393.java
+++ Algorithm/Baekjoon/weeks3/Baekjoon_8393.java
@@ -3,22 +3,22 @@ package baekjoon.weeks3;
 import java.util.Scanner;
 /*
 [백준 알고리즘 - 반복문 단계 3 - 합]
-메모리 : 17652KB
-시간 : 216ms
-코드 길이 : 279B  
+메모리 : 17692KB
+시간 : 200ms
+코드 길이 : 316B  
  */
 public class Baekjoon_8393 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        sc.close();
+
         int sum = 0;
 
         for (int i = 1; i <= n; i++) {
             sum += i;
         }
         System.out.println(sum);
-
-        sc.close();
     }
 }
