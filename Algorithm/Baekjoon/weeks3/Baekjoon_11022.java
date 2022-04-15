c64856e1b9344cc3604f7e2e46e5bdf3af27fb19
eliily00 <wjddms0700@gmail.com>
2022-04-16 00:31:01 +0900

    22.04.16 : [11022 - A+B - 8]

---
+++ Algorithm/Baekjoon/weeks3/Baekjoon_11022.java
@@ -0,0 +1,46 @@
+package baekjoon.weeks3;
+
+import java.util.Scanner;
+/*
+[백준 알고리즘 - 반복문 단계 8 - A+B - 8]
+메모리 : 18952KB
+시간 : 276ms
+코드 길이 : 383B
+    ↓ BufferedReader + StringBuilder
+메모리 : 14188KB
+시간 : 124ms
+코드 길이 : 859B
+ */
+public class Baekjoon_11022 {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     int T = sc.nextInt();
+
+    //     for(int i = 1; i <= T; i++) {
+    //         int A = sc.nextInt();
+    //         int B = sc.nextInt();
+    //         System.out.println("Case #" + i + ": " + A + " + " B + " = " + (A+B));
+    //     }
+    // }
+
+    // BufferedReader + StringBuilder 사용
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #").append(i).append(": ")
+            .append(A).append(" + ").append(B).append(" = ").append((A+B)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
