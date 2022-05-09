4540c996efeb6aa8eda3898989ea20317f5702f5
eliily00 <wjddms0700@gmail.com>
2022-05-09 23:17:38 +0900

    22.05.09 : [10809 - 알파벳 찾기] Modified File

--- Algorithm/Baekjoon/weeks7/Baekjoon_10809.java
+++ Algorithm/Baekjoon/weeks7/Baekjoon_10809.java
@@ -27,9 +27,10 @@ public class Baekjoon_10809 {
     //     String S = sc.nextLine();   // 입력 단어
 
     //     for(int i = 0; i < S.length(); i++) { // arr 원소 값이 -1인 경우 초기화
-    //         char ch = S.charAt(i);
+    //         char ch = S.charAt(i);   
+              // 문자형 변수 ch를 선언하고 S.char(0)부터 S.char(S.length()-1)까지 문자열의 문자들을 한 글자씩 저장
 
-    //         if(arr[ch - 'a'] == -1) {
+    //         if(arr[ch - 'a'] == -1) {    // arr[ch = 'a'] 값이 -1일때 괄호안의 문장 실행
     //             arr[ch - 'a'] = i;
     //         }
     //     }
