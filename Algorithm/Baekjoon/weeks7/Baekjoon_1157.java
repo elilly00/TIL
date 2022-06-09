ac40676151bea4c324b2eb77a6ec4a2bcfa1f857
eliily00 <wjddms0700@gmail.com>
2022-06-09 22:51:54 +0900

    22.05.09 : [1157 - 단어 공부] Modified File

--- Algorithm/Baekjoon/weeks7/Baekjoon_1157.java
+++ Algorithm/Baekjoon/weeks7/Baekjoon_1157.java
@@ -6,25 +6,52 @@ import java.util.Scanner;
 메모리 : 28040KB
 시간 : 332ms
 코드 길이 : 695B
+    ↓ BufferedReader
+메모리 : 24456KB
+시간 : 204ms
+코드 길이 : 805B
  */
 public class Baekjoon_1157 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
         
-        String str = sc.next().toUpperCase();
+    //     String str = sc.next().toUpperCase();
+    //     int[] count = new int[26];
+    //     for (int i = 0; i < str.length(); i++) {
+    //         int num = str.charAt(i) -'A' ;
+    //         count[num]++;
+    //     }
+
+    //     int max = 0;
+    //     char answer = '?';
+    //     for (int i = 0; i < count.length; i++) {
+    //         if(max < count[i]){
+    //             max = count[i];
+    //             answer = (char)(i+'A');
+    //         } else if (max == count[i]){
+    //             answer = '?';
+    //         }
+    //     }
+    //     System.out.println(answer);
+    // }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().toUpperCase();
         int[] count = new int[26];
-        for (int i = 0; i < str.length(); i++) {
-            int num = str.charAt(i) -'A' ;
+        for(int i = 0; i < str.length(); i++) {
+            int num = str.charAt(i) - 'A';
             count[num]++;
         }
 
         int max = 0;
         char answer = '?';
-        for (int i = 0; i < count.length; i++) {
-            if(max < count[i]){
-                max = count[i];
-                answer = (char)(i+'A');
-            } else if (max == count[i]){
+        for(int i = 0; i < count.length; i++) {
+            if(max < count[i]) {
+            max = count[i];
+            answer = (char)(i+'A');
+            } else if(max == count[i]) {
                 answer = '?';
             }
         }
