9166f98642b214f6a4e02cdc2b46959dc45d3116
eliily00 <wjddms0700@gmail.com>
2022-06-17 22:44:17 +0900

    22.06.17 : [2292 - 벌집]

---
+++ Algorithm/Baekjoon/weeks8/Baekjoon_2292.java
@@ -0,0 +1,30 @@
+package baekjoon.weeks8;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+/*
+[백준 알고리즘 - 기본 수학1 단계1  - 벌집]
+메모리 : KB
+시간 : ms
+코드 길이 : B
+ */
+public class Baekjoon_2292 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		int count = 1; // 겹 수(최소 루트)
+		int range = 2;	// 범위 (최솟값 기준) 
+ 
+		if (N == 1) {
+			System.out.print(1);
+        } else {
+			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
+				range = range + (6 * count);
+				count++;
+			}
+			System.out.print(count);
+		}
+	}
+}
