a9501865ffa5ba909fb920c29d4920f07cffada3
eliily00 <wjddms0700@gmail.com>
2022-08-21 23:03:21 +0900

    22.08.21 : [10757 - 큰수 A+B]

--- Algorithm/baekjoon/step7/Baekjoon_10757.java
+++ Algorithm/baekjoon/step7/Baekjoon_10757.java
@@ -2,21 +2,26 @@ package baekjoon.step7;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 import java.io.IOException;
 
 /*
 [백준 알고리즘 - 기본 수학1 단계 8 - 큰수 A+B]
-메모리 : KB
-시간 : ms
-코드 길이 : B
+메모리 : 15056KB
+시간 : 192ms
+코드 길이 : 601B
  */
 public class Baekjoon_10757 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int A = Integer.parseInt(br.readLine());
-        int B = Integer.parseInt(br.readLine());
-
-        System.out.println(A+B);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        
+        A = A.add(B);
+        
+        System.out.println(A.toString());
     }
 }
