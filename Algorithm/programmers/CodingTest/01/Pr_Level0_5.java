95bbf5333f3a0e2022b7cf784d30ffd47e05043c
jepark <wjddms0700@gmail.com>
2023-11-10 15:35:24 +0900

    23.11.10 : [coding Test] level 0

---
+++ Algorithm/programmers/CodingTest/01/Pr_Level0_5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/*
+ 두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+ a + b = c
+
+ 출력 예시
+ 4 + 5 = 9
+ */
+public class Pr_Level0_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+        
+        System.out.println(a+" + "+b+" = "+c);
+    }
+}
